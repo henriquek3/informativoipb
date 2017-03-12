82f75ef6b7d469ff242927ba0f37c14c15ce6a38
Jean Freitas <henriquek3@live.com>
2017-03-12 15:01:37 -0400

    Confirmado cadastros Ministros e Secretários

---
+++ views/consultas/estatistica/iframe-1.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jean Freitas
+ * Date: 12/03/2017
+ * Time: 13:55
+ */
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- <link rel="stylesheet" href="../../vendor/semantic/components/table.css"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.9/semantic.min.css">
+    <style>
+
+    </style>
+</head>
+<body>
+<table class="ui definition celled table">
+    <thead>
+    <tr>
+        <th class="two wide field"></th>
+        <th class="one wide field">Pastores</th>
+        <th class="one wide field">Licenciados</th>
+        <th class="one wide field">Presbíteros</th>
+        <th class="one wide field">Diáconos</th>
+        <th class="one wide field">Evangelistas</th>
+        <th class="one wide field">Missionários</th>
+        <th class="one wide field">Candidatos</th>
+        <th class="one wide field">Igrejas</th>
+        <th class="three wide field">Congreg. de Igrejas</th>
+        <th class="one wide field">Pontos de Pregação</th>
+        <th class="one wide field">N.º Escolas Dom.</th>
+        <th class="one wide field">N.º Prof. da ED</th>
+        <th class="one wide field">N.º Alunos ED - Atual</th>
+        <th class="one wide field">N.º Alunos ED - Anter.</th>
+        <th class="one wide field">Variação Percentual</th>
+        <th class="one wide field">Congreg. Presbiteriais</th>
+        <th class="one wide field">N.º de Educandários</th>
+        <th class="one wide field">N.º de Alunos Educ.</th>
+        <th class="one wide field">N.º de Creches</th>
+        <th class="one wide field">N.º de Crianças</th>
+        <th class="one wide field">N.º Outros</th>
+        <th class="one wide field">N.º de Pessoas</th>
+        <th class="one wide field">N.º UCPs</th>
+        <th class="one wide field">Sócios UCP</th>
+        <th class="one wide field">N.º UPA</th>
+        <th class="one wide field">Sócios UPA</th>
+        <th class="one wide field">N.º UMP</th>
+        <th class="one wide field">Sócios UMP</th>
+        <th class="one wide field">N.º SAF</th>
+        <th class="one wide field">Sócias SAF</th>
+        <th class="one wide field">UPH</th>
+        <th class="one wide field">Sócios UPH</th>
+        <th class="one wide field">N.º Outros</th>
+        <th class="one wide field">Sócios Outros</th>
+        <th class="one wide field">Total de Sociedades</th>
+        <th class="one wide field">Total de Sócios</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>Igreja 1</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>Igreja 1</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+    </tr>
+    <tr>
+        <td>Igreja 1</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>Igreja 1</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+    </tr>
+    </tbody>
+</table>
+
+</body>
+</html>
