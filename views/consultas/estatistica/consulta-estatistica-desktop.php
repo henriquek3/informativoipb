f03b0390992d43e250f793cdd427297b5f51e8c2
Jean Freitas <henriquek3@live.com>
2017-03-12 16:31:07 -0400

    Confirmado cadastros Ministros e Secretários

--- views/consultas/estatistica/consulta-estatistica-desktop.php
+++ views/consultas/estatistica/consulta-estatistica-desktop.php
@@ -5,3 +5,66 @@
  * Date: 05/03/2017
  * Time: 11:53
  */
+?>
+
+
+<div class="ui segment">
+    <table class="ui green definition celled table">
+        <thead>
+        <tr>
+            <th class="two wide field"></th>
+            <th class="one wide field">Valor1</th>
+            <th class="one wide field">Valor2</th>
+            <th class="one wide field">Valor1</th>
+            <th class="one wide field">Valor2</th>
+            <th class="one wide field">Valor1</th>
+            <th class="one wide field">Valor2</th>
+            <th class="one wide field">Valor1</th>
+            <th class="one wide field">Valor2</th>
+            <th class="one wide field">Valor1</th>
+            <th class="one wide field">Valor2</th>
+            <th class="one wide field">Valor1</th>
+            <th class="one wide field">Valor2</th>
+            <th class="one wide field">Valor1</th>
+            <th class="one wide field">Valor2</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Igreja 1</td>
+            <td>200</td>
+            <td>0g</td>
+            <td>200</td>
+            <td>0g</td>
+            <td>200</td>
+            <td>0g</td>
+            <td>200</td>
+            <td>0g</td>
+            <td>200</td>
+            <td>0g</td>
+            <td>200</td>
+            <td>0g</td>
+            <td>200</td>
+            <td>0g</td>
+        </tr>
+        <tr>
+            <td>Igreja 1</td>
+            <td>200</td>
+            <td>0g</td>
+            <td>200</td>
+            <td>0g</td>
+            <td>200</td>
+            <td>0g</td>
+            <td>200</td>
+            <td>0g</td>
+            <td>200</td>
+            <td>0g</td>
+            <td>200</td>
+            <td>0g</td>
+            <td>200</td>
+            <td>0g</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+
---
+++ views/consultas/estatistica/iframe-1-bkp.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jean Freitas
+ * Date: 12/03/2017
+ * Time: 15:01
+ */
+?>
+
+<table class="ui definition celled table">
+    <thead>
+    <tr>
+        <th class="two wide field"></th>
+        <th class="one wide field">Pastores</th>
+        <th class="one wide field">Licenciados</th>
+        <th class="one wide field">Presbíteros</th>
+        <th class="one wide field">Diáconos</th>
+        <th class="one wide field">Evangelistas</th>
+        <th class="one wide field">Missionários</th>
+        <th class="one wide field">Candidatos</th>
+        <th class="one wide field">Igrejas</th>
+        <th class="three wide field">Congreg. de Igrejas</th>
+        <th class="one wide field">Pontos de Pregação</th>
+        <th class="one wide field">N.º Escolas Dom.</th>
+        <th class="one wide field">N.º Prof. da ED</th>
+        <th class="one wide field">N.º Alunos ED - Atual</th>
+        <th class="one wide field">N.º Alunos ED - Anter.</th>
+        <th class="one wide field">Variação Percentual</th>
+        <th class="one wide field">Congreg. Presbiteriais</th>
+        <th class="one wide field">N.º de Educandários</th>
+        <th class="one wide field">N.º de Alunos Educ.</th>
+        <th class="one wide field">N.º de Creches</th>
+        <th class="one wide field">N.º de Crianças</th>
+        <th class="one wide field">N.º Outros</th>
+        <th class="one wide field">N.º de Pessoas</th>
+        <th class="one wide field">N.º UCPs</th>
+        <th class="one wide field">Sócios UCP</th>
+        <th class="one wide field">N.º UPA</th>
+        <th class="one wide field">Sócios UPA</th>
+        <th class="one wide field">N.º UMP</th>
+        <th class="one wide field">Sócios UMP</th>
+        <th class="one wide field">N.º SAF</th>
+        <th class="one wide field">Sócias SAF</th>
+        <th class="one wide field">UPH</th>
+        <th class="one wide field">Sócios UPH</th>
+        <th class="one wide field">N.º Outros</th>
+        <th class="one wide field">Sócios Outros</th>
+        <th class="one wide field">Total de Sociedades</th>
+        <th class="one wide field">Total de Sócios</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>Igreja 1</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>Igreja 1</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+    </tr>
+    <tr>
+        <td>Igreja 1</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>Igreja 1</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+        <td>200</td>
+        <td>0g</td>
+    </tr>
+    </tbody>
+</table>
