137e4b6a64d7e820c7e9a64278cd6331a069e501
Jean Freitas <henriquek3@live.com>
2017-03-12 22:23:36 -0400

    Confirmado cadastros Ministros e Secretários

--- views/consultas/estatistica/iframe-1-bkp.php
+++ views/consultas/estatistica/iframe-1-bkp.php
@@ -3,130 +3,167 @@
  * Created by PhpStorm.
  * User: Jean Freitas
  * Date: 12/03/2017
- * Time: 15:01
+ * Time: 13:55
  */
 ?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../vendor/semantic/components/table.css">
+    <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.9/semantic.min.css">-->
+    <style>
+        .col-min-width-21c {
+            min-width: 140px;
+        }
 
+        .col-min-width-20c {
+            min-width: 135px;
+        }
+
+        .col-min-width-15c {
+            min-width: 112px;
+        }
+
+        .col-min-width-12c {
+            min-width: 90px;
+        }
+
+        .col-min-width-10c {
+            min-width: 80px;
+        }
+
+        .thgreen {
+            border-top: 0.2em solid #21BA45;
+        }
+    </style>
+</head>
+<body>
 <table class="ui definition celled table">
     <thead>
     <tr>
-        <th class="two wide field"></th>
-        <th class="one wide field">Pastores</th>
-        <th class="one wide field">Licenciados</th>
-        <th class="one wide field">Presbíteros</th>
-        <th class="one wide field">Diáconos</th>
-        <th class="one wide field">Evangelistas</th>
-        <th class="one wide field">Missionários</th>
-        <th class="one wide field">Candidatos</th>
-        <th class="one wide field">Igrejas</th>
-        <th class="three wide field">Congreg. de Igrejas</th>
-        <th class="one wide field">Pontos de Pregação</th>
-        <th class="one wide field">N.º Escolas Dom.</th>
-        <th class="one wide field">N.º Prof. da ED</th>
-        <th class="one wide field">N.º Alunos ED - Atual</th>
-        <th class="one wide field">N.º Alunos ED - Anter.</th>
-        <th class="one wide field">Variação Percentual</th>
-        <th class="one wide field">Congreg. Presbiteriais</th>
-        <th class="one wide field">N.º de Educandários</th>
-        <th class="one wide field">N.º de Alunos Educ.</th>
-        <th class="one wide field">N.º de Creches</th>
-        <th class="one wide field">N.º de Crianças</th>
-        <th class="one wide field">N.º Outros</th>
-        <th class="one wide field">N.º de Pessoas</th>
-        <th class="one wide field">N.º UCPs</th>
-        <th class="one wide field">Sócios UCP</th>
-        <th class="one wide field">N.º UPA</th>
-        <th class="one wide field">Sócios UPA</th>
-        <th class="one wide field">N.º UMP</th>
-        <th class="one wide field">Sócios UMP</th>
-        <th class="one wide field">N.º SAF</th>
-        <th class="one wide field">Sócias SAF</th>
-        <th class="one wide field">UPH</th>
-        <th class="one wide field">Sócios UPH</th>
-        <th class="one wide field">N.º Outros</th>
-        <th class="one wide field">Sócios Outros</th>
-        <th class="one wide field">Total de Sociedades</th>
-        <th class="one wide field">Total de Sócios</th>
+        <th class=" wide field col-min-width-21c"></th>
+        <th class=" wide field thgreen">Pastores</th>
+        <th class=" wide field thgreen">Licenciados</th>
+        <th class=" wide field thgreen">Presbíteros</th>
+        <th class=" wide field thgreen">Diáconos</th>
+        <th class=" wide field thgreen">Evangelistas</th>
+        <th class=" wide field thgreen">Missionários</th>
+        <th class=" wide field thgreen">Candidatos</th>
+        <th class=" wide field thgreen">Igrejas</th>
+        <th class=" wide field col-min-width-15c thgreen">Congr. Igrejas</th>
+        <th class=" wide field col-min-width-15c thgreen">Pontos Pregação</th>
+        <th class=" wide field  thgreen">E.D.</th>
+        <th class=" wide field col-min-width-10c thgreen">Prof. E.D.</th>
+        <th class=" wide field col-min-width-12c thgreen">Alunos E.D.</th>
+        <th class=" wide field col-min-width-21c thgreen">Alunos E.D. - Anter.</th>
+        <th class=" wide  field col-min-width-10c thgreen">Variação %</th>
+        <th class=" wide  field col-min-width-20c thgreen">Congr. Presbiteriais</th>
+        <th class=" wide  field  thgreen">Educandários</th>
+        <th class=" wide  field col-min-width-15c thgreen">Alunos Educ.</th>
+        <th class=" wide  field  thgreen">Creches</th>
+        <th class=" wide  field  thgreen">Crianças</th>
+        <th class=" wide  field  thgreen">Outros</th>
+        <th class=" wide  field  thgreen">Pessoas</th>
+        <th class=" wide  field  thgreen">UCPs</th>
+        <th class=" wide  field col-min-width-12c thgreen">Sócios UCP</th>
+        <th class=" wide  field  thgreen">UPA</th>
+        <th class=" wide  field col-min-width-12c thgreen">Sócios UPA</th>
+        <th class=" wide  field  thgreen">UMP</th>
+        <th class=" wide  field col-min-width-12c thgreen">Sócios UMP</th>
+        <th class=" wide  field  thgreen">SAF</th>
+        <th class=" wide  field col-min-width-12c thgreen">Sócias SAF</th>
+        <th class=" wide  field  thgreen">UPH</th>
+        <th class=" wide  field col-min-width-12c thgreen">Sócios UPH</th>
+        <th class=" wide  field  thgreen">Outros</th>
+        <th class=" wide  field col-min-width-15c thgreen">Sócios Outros</th>
+        <th class=" wide  field col-min-width-20c thgreen">Total de Sociedades</th>
+        <th class=" wide  field col-min-width-15c thgreen">Total de Sócios</th>
     </tr>
     </thead>
     <tbody>
     <tr>
         <td>Igreja 1</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>Igreja 1</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">Igreja 1</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
     </tr>
     <tr>
         <td>Igreja 1</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>Igreja 1</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
-        <td>200</td>
-        <td>0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">Igreja 1</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
+        <td class="center aligned">200</td>
+        <td class="center aligned">0g</td>
     </tr>
     </tbody>
 </table>
+</body>
+</html>
