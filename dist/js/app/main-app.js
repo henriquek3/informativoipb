f0ea61f98980d523764cdbef596a075a787e0181
Jean Freitas <henriquek3@live.com>
2018-02-14 15:21:27 -0300

    Delete main-app.js

--- dist/js/app/main-app.js
+++
@@ -1,6 +0,0 @@
-/**
- * @description Aqui será implementado o javascript que será utilizado em todas as telas, por exemplo verificação de login.
- *
- */
-"use strict";
-//# sourceMappingURL=main-app.js.map
