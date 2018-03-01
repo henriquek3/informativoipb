7e78cdb47a2ae4d2c11ec34c3ac0915f5f11dfe2
Jean Freitas <henriquek3@live.com>
2018-03-01 08:42:02 -0400

    atualização geral

--- src/routes.php
+++ src/routes.php
@@ -8,14 +8,17 @@
 
 $app->get('/{routes}', 
     function ($routes) {
-        ob_start();
+        ob_start();        
         $file = __DIR__ . "/../templates/{$routes}.html";
         if ( file_exists($file) ) {
             include $file;
+            return ob_get_clean();
         } elseif ($routes == 'inicio') {
             include __DIR__ . '/../templates/index.html';
+            return ob_get_clean();
         } else {
             include __DIR__ . '/../templates/page-error.html';
+            return ob_get_clean();
         }
-    return ob_get_clean();
+    return true;
 });
