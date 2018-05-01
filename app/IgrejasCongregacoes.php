4f2dd7350a9b60212639c1394759f45556a6d2ad
Jean Freitas <henriquek3@live.com>
2018-05-01 16:55:08 -0400

    email, cnpj congreg

--- app/IgrejasCongregacoes.php
+++ app/IgrejasCongregacoes.php
@@ -10,7 +10,7 @@ class IgrejasCongregacoes extends Model
     use SoftDeletes;
 
     protected $guarded = [
-        'id', 'created_at', 'deleted_at', 'updated_at',
+        'id', 'created_at', 'deleted_at', 'updated_at', 'cnpj',
     ];
 
     public function usuario()
