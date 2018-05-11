830c25612550114a32632693657519d34c489401
Jean Freitas <henriquek3@live.com>
2018-05-11 11:37:34 -0400

    migrate:fresh

--- database/migrations/2018_04_14_183237_create_igrejas_congregacoes_table.php
+++ database/migrations/2018_04_14_183237_create_igrejas_congregacoes_table.php
@@ -30,7 +30,7 @@ class CreateIgrejasCongregacoesTable extends Migration
             $table->string('telefone', 20)->nullable();
             $table->string('email', 75)->nullable();
             $table->string('website')->nullable();
-            $table->integer('user_id');
+            $table->unsignedInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->softDeletes();
             $table->timestamps();
