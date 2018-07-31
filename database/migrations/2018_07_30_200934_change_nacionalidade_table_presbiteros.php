01a8ce09b7464cacf01ddbb91953b099af7c8414
Jean Freitas <henriquek3@live.com>
2018-07-30 20:18:40 -0400

    alterando nacionalidade datatype

---
+++ database/migrations/2018_07_30_200934_change_nacionalidade_table_presbiteros.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeNacionalidadeTablePresbiteros extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('presbiteros', function (Blueprint $table) {
+            $table->string('nacionalidade', 191)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('presbiteros', function (Blueprint $table) {
+            $table->integer('nacionalidade')->change();
+        });
+    }
+}
