5b3eccb1eec43d9405740818e8f5ff7805eecc07
Jean Freitas <henriquek3@live.com>
2018-08-30 12:27:30 -0400

    pastas reuniao

---
+++ resources/views/pages/reuniao-conselhos/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.informativo')
+@section('css')@endsection
+@section('content')
+    <h1> Reuniao Conselhos</h1>
+@endsection
+@section('javascript')
+
+@endsection
---
+++ resources/views/pages/reuniao-estatisticas/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.informativo')
+@section('css')@endsection
+@section('content')
+    <h1> Reuniao Estatisticas</h1>
+@endsection
+@section('javascript')
+
+@endsection
