31b966fe66e6ac066331473a551c3b371d123c7e
Jean Freitas <henriquek3@live.com>
2018-09-05 16:05:14 -0400

    checked

--- resources/views/pages/presbiteros/form.blade.php
+++ resources/views/pages/presbiteros/form.blade.php
@@ -59,7 +59,8 @@
                             <div class="ui checkbox"
                                  style="float: left; display: flex; margin-top: 32px; margin-left: 20px;">
                                 <input name="pastor_titular" type="checkbox"
-                                       value="1" {{$resource->pastor_titular === 1 ? ' checked' : ''}}>
+                                       {{$resource->pastor_titular == 1 ? ' checked' : ''}}
+                                       value="1">
                                 <label>Pastor Títular</label>
                             </div>
                         </div>
