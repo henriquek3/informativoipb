0994627855944554a2a3ed9cfa4deab806a14d5f
kallewstudio@live.com <amofoto.com182>
2018-09-05 11:35:16 -0400

    Revisão das funções #29

--- resources/views/pages/relatorios-estatisticas/form.blade.php
+++ resources/views/pages/relatorios-estatisticas/form.blade.php
@@ -24,11 +24,11 @@
                     </div>
                     <div class="seven wide field">
                         <label>Sínodo</label>
-                        <input type="text" name="sinodo" value="{{auth()->user()->presbitero->igreja->presbiterio->sinodo->nome}}" disabled="">
+                        <input type="text" name="sinodo" value="{{auth()->user()->presbitero->igreja->presbiterio->sinodo->nome}}" readonly="">
                     </div>
                     <div class="seven wide field" id="div_presbiterio">
                         <label>Presbitério</label>
-                        <input type="text" name="presbiterio" value="{{auth()->user()->presbitero->igreja->presbiterio->nome}}" disabled="">
+                        <input type="text" name="presbiterio" value="{{auth()->user()->presbitero->igreja->presbiterio->nome}}" readonly="">
                     </div>
 
                 </div>
@@ -46,69 +46,69 @@
                     </div>
                     <div class="fourteen wide field">
                         <label>Nome (Igreja/Congregação)</label>
-                        <input type="text" disabled="" name="nome_igreja" value="{{auth()->user()->presbitero->igreja->nome}}">
+                        <input type="text" readonly="" name="nome_igreja" value="{{auth()->user()->presbitero->igreja->nome}}">
                     </div>
                 </div>
                 <div class="fields">
                     <div class="eight wide field">
                         <label>Endereço</label>
-                        <input type="text" disabled="" name="endereco" value="{{auth() ->user() ->presbitero->igreja->endereco}}">
+                        <input type="text" readonly="" name="endereco" value="{{auth() ->user() ->presbitero->igreja->endereco}}">
                     </div>
                     <div class="two wide field">
                         <label>Número</label>
-                        <input type="text" disabled="" name="endereco_numero" value="{{auth() ->user() ->presbitero->igreja->endereco_numero}}">
+                        <input type="text" readonly="" name="endereco_numero" value="{{auth() ->user() ->presbitero->igreja->endereco_numero}}">
                     </div>
                     <div class="three wide field">
                         <label>Complemento</label>
-                        <input type="text" disabled="" name="endereco_complemento" value="{{auth() ->user() ->presbitero->igreja->endereco_complemento}}">
+                        <input type="text" readonly="" name="endereco_complemento" value="{{auth() ->user() ->presbitero->igreja->endereco_complemento}}">
                     </div>
                     <div class="three wide field">
                         <label>Bairro</label>
-                        <input type="text" disabled="" name="endereco_bairro"  value="{{auth() ->user() ->presbitero->igreja->endereco_bairro}}">
+                        <input type="text" readonly="" name="endereco_bairro"  value="{{auth() ->user() ->presbitero->igreja->endereco_bairro}}">
                     </div>
                 </div>
                 <div class="fields">
                     <div class="four wide field">
                         <label>Estado</label>
-                        <input type="text" disabled="" name="estado"  value="{{auth() ->user() ->presbitero->igreja->cidade->estado->nome}}">
+                        <input type="text" readonly="" name="estado"  value="{{auth() ->user() ->presbitero->igreja->cidade->estado->nome}}">
                     </div>
                     <div class="four wide field">
                         <label>Cidade</label>
-                        <input type="text" disabled="" name="cidade"  value="{{auth() ->user() ->presbitero->igreja->cidade->nome}}">
+                        <input type="text" readonly="" name="cidade"  value="{{auth() ->user() ->presbitero->igreja->cidade->nome}}">
                     </div>
                     <div class="three wide field">
                         <label>CEP</label>
-                        <input type="text" disabled="" name="endereco_cep"  value="{{auth() ->user() ->presbitero->igreja->endereco_cep}}">
+                        <input type="text" readonly="" name="endereco_cep"  value="{{auth() ->user() ->presbitero->igreja->endereco_cep}}">
                     </div>
                     <div class="two wide field">
                         <label>Cx. P</label>
-                        <input type="text" disabled="" name="endereco_cx_postal"  value="{{auth() ->user() ->presbitero->igreja->endereco_cx_postal}}">
+                        <input type="text" readonly="" name="endereco_cx_postal"  value="{{auth() ->user() ->presbitero->igreja->endereco_cx_postal}}">
                     </div>
                     <div class="three wide field">
                         <label>CEP Cx. P</label>
-                        <input type="text" disabled="" name="endereco_cx_postal_cep"  value="{{auth() ->user() ->presbitero->igreja->endereco_cx_postal_cep}}">
+                        <input type="text" readonly="" name="endereco_cx_postal_cep"  value="{{auth() ->user() ->presbitero->igreja->endereco_cx_postal_cep}}">
                     </div>
                 </div>
                 <div class="fields">
                     <div class="two wide field">
                         <label>Telefone</label>
-                        <input type="text" disabled="" name="telefone"  value="{{auth() ->user() ->presbitero->igreja->telefone}}">
+                        <input type="text" readonly="" name="telefone"  value="{{auth() ->user() ->presbitero->igreja->telefone}}">
                     </div>
                     <div class="four wide field">
                         <label>E-Mail</label>
-                        <input type="text" disabled="" name="email"  value="{{auth() ->user() ->presbitero->igreja->email}}">
+                        <input type="text" readonly="" name="email"  value="{{auth() ->user() ->presbitero->igreja->email}}">
                     </div>
                     <div class="four wide field">
                         <label>HomePage</label>
-                        <input type="text" disabled="" name="homepage"  value="{{auth() ->user() ->presbitero->igreja->website}}">
+                        <input type="text" readonly="" name="homepage"  value="{{auth() ->user() ->presbitero->igreja->website}}">
                     </div>
                     <div class="three wide field">
                         <label>CNPJ</label>
-                        <input type="text" disabled="" name="cnpj"  value="{{auth() ->user() ->presbitero->igreja->cnpj}}">
+                        <input type="text" readonly="" name="cnpj"  value="{{auth() ->user() ->presbitero->igreja->cnpj}}">
                     </div>
                     <div class="three wide field">
                         <label>Data Organização</label>
-                        <input type="text" disabled="" name="data_organizacao"  value="{{auth() ->user() ->presbitero->igreja->data_organizacao}}">
+                        <input type="text" readonly="" name="data_organizacao"  value="{{auth() ->user() ->presbitero->igreja->data_organizacao}}">
                     </div>
                 </div>
             </div>
@@ -1194,86 +1194,89 @@
                     </div>
                     <div class="fourteen wide field">
                         <label>Secretário do Conselho</label>
-                        <input type="text" name="relator_nome" disabled="" value="{{auth()->user()->presbitero->nome}}">
+                        <input type="text" name="relator_nome" readonly="" value="{{auth()->user()->presbitero->nome}}">
                     </div>
                 </div>
                 <div class="fields">
                     <div class="eight wide field">
                         <label>Endereço</label>
-                        <input type="text" name="relator_endereco" disabled="" value="{{auth()->user()->presbitero->endereco}}">
+                        <input type="text" name="relator_endereco" readonly="" value="{{auth()->user()->presbitero->endereco}}">
                     </div>
                     <div class="two wide field">
                         <label>Número</label>
-                        <input type="text" name="relator_endereco_numero" disabled="" value="{{auth()->user()->presbitero->endereco_nr}}">
+                        <input type="text" name="relator_endereco_numero" readonly="" value="{{auth()->user()->presbitero->endereco_nr}}">
                     </div>
                     <div class="three wide field">
                         <label>Complemento</label>
-                        <input type="text" name="relator_endereco_complemento" disabled="" value="{{auth()->user()->presbitero->endereco_complemento}}">
+                        <input type="text" name="relator_endereco_complemento" readonly="" value="{{auth()->user()->presbitero->endereco_complemento}}">
                     </div>
                     <div class="three wide field">
                         <label>Bairro</label>
-                        <input type="text" name="relator_endereco_bairro" disabled="" value="{{auth()->user()->presbitero->endereco_bairro}}">
+                        <input type="text" name="relator_endereco_bairro" readonly="" value="{{auth()->user()->presbitero->endereco_bairro}}">
                     </div>
                 </div>
                 <div class="fields">
                     <div class="four wide field">
                         <label>Estado</label>
-                        <input type="text" name="relator_estado" disabled="" value="{{auth()->user()->presbitero->endereco_id_estado}}">
+                        <input type="text" name="relator_estado" readonly="" value="{{auth()->user()->presbitero->endereco_id_estado}}">
                     </div>
                     <div class="four wide field">
                         <label>Cidade</label>
-                        <input type="text" name="relator_cidade" disabled="" value="{{auth()->user()->presbitero->endereco_id_cidade}}">
+                        <input type="text" name="relator_cidade" readonly="" value="{{auth()->user()->presbitero->endereco_id_cidade}}">
                     </div>
                     <div class="three wide field">
                         <label>CEP</label>
-                        <input type="text" name="relator_endereco_cep" disabled="" value="{{auth()->user()->presbitero->cep}}">
+                        <input type="text" name="relator_endereco_cep" readonly="" value="{{auth()->user()->presbitero->cep}}">
                     </div>
                     <div class="two wide field">
                         <label>Cx. P</label>
-                        <input type="text" name="relator_cx" disabled="" value="{{auth()->user()->presbitero->cx_postal}}">
+                        <input type="text" name="relator_cx" readonly="" value="{{auth()->user()->presbitero->cx_postal}}">
                     </div>
                     <div class="three wide field">
                         <label>CEP Cx. P</label>
-                        <input type="text" name="relator_cep_cx" disabled="" value="{{auth()->user()->presbitero->cx_postal_cep}}">
+                        <input type="text" name="relator_cep_cx" readonly="" value="{{auth()->user()->presbitero->cx_postal_cep}}">
                     </div>
                 </div>
                 <div class="fields">
                     <div class="three wide field">
                         <label>Telefone</label>
-                        <input type="text" name="relator_telefone" disabled="" value="{{auth()->user()->presbitero->telefone}}">
+                        <input type="text" name="relator_telefone" readonly="" value="{{auth()->user()->presbitero->telefone}}">
                     </div>
                     <div class="three wide field">
                         <label>Celular</label>
-                        <input type="text" name="relator_celular" disabled="" value="{{auth()->user()->presbitero->celular}}">
+                        <input type="text" name="relator_celular" readonly="" value="{{auth()->user()->presbitero->celular}}">
                     </div>
                     <div class="three wide field">
                         <label>Tel. Igreja</label>
-                        <input type="text" name="relator_igreja_telefone" disabled="" value="{{auth()->user()->presbitero->igreja->telefone}}">
+                        <input type="text" name="relator_igreja_telefone" readonly="" value="{{auth()->user()->presbitero->igreja->telefone}}">
                     </div>
                     <div class="seven wide field">
                         <label>E-mail</label>
-                        <input type="text" name="relator_email" disabled="" value="{{auth()->user()->presbitero->email}}">
+                        <input type="text" name="relator_email" readonly="" value="{{auth()->user()->presbitero->email}}">
                     </div>
                 </div>
             </div>
         </div>
-            <div class="ui segments">
-                <div class="ui horizontal segments">
-                    <div class="ui segment"><span
-                                style="color: lightslategray;"><strong>Usuário:</strong></span><span
-                                style="color: lightslategray;">&nbsp; {{ isset($resource) === true ? $resource->usuario->nome : ''}}
-                            <span id="user_inc"></span>&nbsp;</span><span
-                                style="float: right;color: lightslategray;"><strong>Data:</strong>&nbsp; {{ isset($resource) === true ? $resource->updated_at->format("d/m/Y h:m") : ''}}
-                            <span id="data_inc"></span></span></div>
+            <div class="ui horizontal segments">
+                <div class="ui segment">
+                    <span><strong>Usuário:</strong></span>
+                    <span>&nbsp; {{ isset($resource) === true ? $resource->usuario->nome : ''}}</span>
+                </div>
+                <div class="ui segment">
+                    <span><strong>Data:</strong>&nbsp; {{ isset($resource) === true ? $resource->updated_at->format("d/m/Y h:m") : ''}}</span>
+                </div>
+                <div class="ui right aligned segment" style="width: 30px;">
+                    <span class="ui toggle checkbox"
+                          data-tooltip="Bloqueia edição e altera o status para finalizado">
+                    <input type="checkbox" name="status_relatorio" value="1" @isset($resource) {{$resource->status_relatorio === 1 ? ' checked' : ''}} @endisset>
+                    <label>Relatório Finalizado</label>
+                </span>
                 </div>
             </div>
             <div class="ui clearing divider"></div>
             <div style="text-align: center">
-                <button class="ui green labeled icon button" type="submit"><i class="plus icon"></i>Salvar</button>
-                <button class="ui reset button" type="reset"><i class="minus icon"></i>Limpar</button>
-                <button class="ui red right labeled icon button" type="submit" form="formDelete"><i
-                            class="remove icon"></i>Excluir
-                </button>
+                <button class="ui green labeled icon button" type="submit"><i class="plus icon"></i>Gravar</button>
+                <button class="ui right labeled icon button" type="reset"><i class="minus icon"></i>Limpar</button>
             </div>
     </form>
 @endsection
