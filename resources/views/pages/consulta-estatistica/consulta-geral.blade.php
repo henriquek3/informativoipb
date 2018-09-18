faa5efc599914bd5c0ba2f929ca85b5002a56296
kallewstudio@live.com <amofoto.com>
2018-09-18 14:07:36 -0400

    Ajustando relatório #22

--- resources/views/pages/consulta-estatistica/consulta-geral.blade.php
+++ resources/views/pages/consulta-estatistica/consulta-geral.blade.php
@@ -23,7 +23,7 @@
                         <div class="ui search" title="Digite o nome do sínodo" id="sinodo_search"
                              @isset($resource) data-tooltip="Sigla: {{strtoupper($resource->presbitero->igreja->presbiterio->sinodo->sigla) ?? ''}}" @endisset>
                             <div class="ui left icon input">
-                                <input class="prompt" type="text" placeholder="Procurar Sínodo" name="sinodo" required
+                                <input class="prompt" type="text" placeholder="Procurar Sínodo" name="sinodo"
                                        @isset($resource)  value="{{$resource->presbitero->igreja->presbiterio->sinodo->nome ?? ''}}" @endisset>
                                 <input type="hidden" name="id_sinodo" value="{{$resource->id_sinodo ?? ''}}">
                                 <i class="search icon"></i>
@@ -35,7 +35,7 @@
                         <div class="ui search" title="Digite o nome do presbitério" id="presbiterio_search"
                              @isset($resource) data-tooltip="Sigla: {{strtoupper($resource->presbitero->igreja->presbiterio->sigla) ?? ''}}" @endisset>
                             <div class="ui left icon input">
-                                <input class="prompt" type="text" placeholder="Procurar Presbitério" required
+                                <input class="prompt" type="text" placeholder="Procurar Presbitério"
                                        name="presbiterio"
                                        @isset($resource)  value="{{$resource->presbitero->igreja->presbiterio->nome ?? ''}}" @endisset>
                                 <input type="hidden" name="id_presbiterio" value="{{$resource->id_presbiterio ?? ''}}">
@@ -47,12 +47,12 @@
                 <div class="fields">
                     <div class="eight wide field" id="div_igreja">
                         <label>Igreja</label>
-                        <select class="ui fluid search dropdown" name="id_igreja" required></select>
+                        <select class="ui fluid search dropdown" name="id_igreja" ></select>
                         <div class="ui active inline small loader" style="display:none" id="loader_igreja"></div>
                     </div>
                     <div class="eight wide field" id="div_presbitero">
                         <label>Presbítero</label>
-                        <select class="ui fluid search dropdown" name="id_presbitero" required></select>
+                        <select class="ui fluid search dropdown" name="id_presbitero" ></select>
                         <div class="ui active inline small loader" style="display:none" id="loader_presbitero"></div>
                     </div>
                 </div>
