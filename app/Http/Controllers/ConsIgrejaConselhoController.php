0029dd156dc8b75bc58cc31c777d84422bba0ef6
Jean Freitas <henriquek3@live.com>
2018-09-18 13:44:33 -0400

    consulta

--- app/Http/Controllers/ConsIgrejaConselhoController.php
+++ app/Http/Controllers/ConsIgrejaConselhoController.php
@@ -43,9 +43,9 @@ class ConsIgrejaConselhoController extends Controller
      * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Request $request)
     {
-        //
+        dd($request->all());
     }
 
     /**
